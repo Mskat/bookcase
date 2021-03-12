7599d586aa7d567f9e008f584595de52f3141019
Kat <zur.katarzyna@gmail.com>
2021-03-12 15:30:22 +0000

    Create modul book

---
+++ src/model/book.js
@@ -0,0 +1,42 @@
+class Book {
+    constructor(strTitle, strAuthor, intNumberOfPages, intReleaseDate) {
+        this._strTitle = strTitle;
+        this._strAuthor = strAuthor;
+        this._intPagesNumber = intNumberOfPages;
+        this._intReleaseDate = intReleaseDate;
+    };
+    
+    getTitle() {
+        return this._strTitle;
+    };
+
+    setTitle(strTitle) {
+        this._strTitle = strTitle;
+    };
+
+    getAuthor() {
+        return this._strAuthor;
+    };
+
+    setAuthor(strAuthor) {
+        this._strAuthor = strAuthor;
+    }
+
+    getNumberOfPages() {
+        return this._intPagesNumber;
+    };
+
+    setNumberOfPages(intNumberOfPages) {
+        this._intPagesNumber = intNumberOfPages;
+    }
+
+    getReleaseData() {
+        return this._intReleaseDate;
+    };
+
+    setReleaseData(intReleaseDate) {
+        this._intReleaseDate = intReleaseDate;
+    }
+}
+
+module.exports = Book;
