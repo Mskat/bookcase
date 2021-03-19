82b024e6e00d82750fe598a94f4b0076a475e342
Kat <zur.katarzyna@gmail.com>
2021-03-19 09:15:05 +0000

    Fix formatting

--- src/controller/input.js
+++ src/controller/input.js
@@ -1,17 +1,16 @@
 const prompt = require('prompt-sync')({ sigint: true });
 
 class Input {
-    static _getInput() {
-        let strInput = prompt('Enter number: ');
-        return strInput;
-    };
+	static _getInput() {
+		let strInput = prompt('Enter number: ');
+		return strInput;
+	}
 
-    static _pressEnterToContinue() {
-        let strInput = prompt('Press enter to try again.\n');
-        return strInput;
-    };
+	static _pressEnterToContinue() {
+		let strInput = prompt('Press enter to try again.\n');
+		return strInput;
+	}
 }
 
 module.exports = Input;
-
 
