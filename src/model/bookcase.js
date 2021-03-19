32b2219fd2f93c34088a5d37bc4afff237112bb3
Kat <zur.katarzyna@gmail.com>
2021-03-19 10:02:08 +0000

    Create Bookcase class

---
+++ src/model/bookcase.js
@@ -0,0 +1,15 @@
+class Bookcase {
+	constructor() {
+		this._arrBooks = [];
+	}
+	setBooks(book) {
+		this._arrBookcase.push(book);
+	}
+
+	getBooks() {
+		return this._arrBookcase;
+	}
+}
+
+module.exports = Bookcase;
+
